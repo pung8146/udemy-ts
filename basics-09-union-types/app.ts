331d569312b0d9a68290d3448b6317b1ffee6d25
pung8146 <pung8146@naver.com>
2023-03-08 16:29:37 +0900

    TS 타입 tuple 이란

--- basics-09-union-types/app.ts
+++ basics-09-union-types/app.ts
@@ -3,7 +3,7 @@ const person: {
   age: number;
   hobbies: string[];
   role: [number, string];
-  // tuple 은 배열안에 명시된 타입을 지정해줘야합니다.
+  // tuple 은 배열안에 타입과 크기를 지정하므로 배열과 용도를 달리 사용할 수 있습니다.
 } = {
   name: "Hoon",
   age: 30,
@@ -12,7 +12,8 @@ const person: {
 };
 
 person.role.push("admin");
+// push는 예외적으로 튜플에서 허용되어 타입스크립트가 오류를 잡아내지 못합니다.
 person.role[1] = 10;
-
 // 튜플에서 [1] 에 저장된 타입은 string 이여서 오류가 출력됩니다
 // 그렇지만 js으로 변환은 이루어집니다.
+person.role = [0, "admin"];
