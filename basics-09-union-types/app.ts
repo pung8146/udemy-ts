47140b867fc553a60bc33b1d1d8d003652d332bd
pung8146 <pung8146@naver.com>
2023-03-09 14:55:38 +0900

    함수 타입에 관련 작성된 코드

--- basics-09-union-types/app.ts
+++ basics-09-union-types/app.ts
@@ -1,31 +1,24 @@
-function combine(
-  input1: number | string,
-  input2: number | string,
-  resultConversion: "as-number" | "as-text"
-) {
-  let result;
-  if (
-    (typeof input1 === "number" && typeof input2 === "number") ||
-    resultConversion === "as-number"
-  ) {
-    result = +input1 + +input2;
-  } else {
-    result = input1.toString() + input2.toString();
-  }
-  return result;
-  // if (resultConversion === "as-number") {
-  //   return +result;
-  // } else {
-  //   return result.toString();
-  // }
+function add(n1: number, n2: number) {
+  return n1 + n2;
 }
 
-const combinedAges = combine(30, 26, "as-number");
-console.log(combinedAges);
-const combinedStringAges = combine("30", "26", "as-number");
-console.log(combinedStringAges);
-const combinedNames = combine("john", "max", "as-text");
-console.log(combinedNames);
+function printResult(num: number): void {
+  console.log("Result: " + num);
+}
 
-// 서로 다른 두 종류의 값을 사용해야 하는 어플리케이션에서
-// 함수나 상수 혹은 변수의 매개 변수
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+  // 아무것도 반환하지 않지만 인수가 number 인 callback 함수 cb작성
+  const result = n1 + n2;
+  cb(result);
+}
+
+printResult(add(5, 12));
+
+addAndHandle(10, 20, (result) => {
+  // result 에 number를 표기 하지않는 이유는 TS가 스스로 추론하기때문에
+  // return 값을 넣어도 아무일이 생기지 않는 이유는 void로 아무일이 없다고 명시했기 때문입니다.
+  console.log(result);
+});
+
+// 함수가 undefined 를 비롯 아무것도 반환하지 않는다면 void를 써야됩니다.
+// TS 에서는 undefined 도 타입입니다.
