9e53ecd5626ddce479801b99c14ca91510e8d5d5
pung8146 <pung8146@naver.com>
2023-03-09 11:45:48 +0900

    리터럴 타입의 활용

--- basics-09-union-types/app.ts
+++ basics-09-union-types/app.ts
@@ -1,14 +1,31 @@
-function combine(input1: number | string, input2: number | string) {
+function combine(
+  input1: number | string,
+  input2: number | string,
+  resultConversion: "as-number" | "as-text"
+) {
   let result;
-  if (typeof input1 === "number" && typeof input2 === "number") {
-    result = input1 + input2;
+  if (
+    (typeof input1 === "number" && typeof input2 === "number") ||
+    resultConversion === "as-number"
+  ) {
+    result = +input1 + +input2;
   } else {
     result = input1.toString() + input2.toString();
   }
   return result;
+  // if (resultConversion === "as-number") {
+  //   return +result;
+  // } else {
+  //   return result.toString();
+  // }
 }
 
-const combinedAges = combine(30, 26);
-const combinedNames = combine("john", "max");
+const combinedAges = combine(30, 26, "as-number");
+console.log(combinedAges);
+const combinedStringAges = combine("30", "26", "as-number");
+console.log(combinedStringAges);
+const combinedNames = combine("john", "max", "as-text");
+console.log(combinedNames);
+
 // 서로 다른 두 종류의 값을 사용해야 하는 어플리케이션에서
 // 함수나 상수 혹은 변수의 매개 변수
