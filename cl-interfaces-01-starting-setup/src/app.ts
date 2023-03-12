0fa6218040f4b6e9f0b9dfa2a9a3a45d37740607
pung8146 <pung8146@naver.com>
2023-03-12 21:48:59 +0900

    tsc 로변환시 출력위치를 dist 로 변경해야 합니다.

--- cl-interfaces-01-starting-setup/src/app.ts
+++ cl-interfaces-01-starting-setup/src/app.ts
@@ -1,1 +1,12 @@
-// Code goes here!
+class Department {
+  name: string;
+
+  constructor(n: string) {
+    this.name = n;
+  }
+  //예약어
+}
+
+const accounting = new Department("Accounting");
+
+console.log(accounting);
