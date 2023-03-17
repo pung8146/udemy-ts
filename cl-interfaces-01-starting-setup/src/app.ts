54661a64c18d6b12528b73ac091e0380576b72ea
pung8146 <pung8146@naver.com>
2023-03-17 10:06:36 +0900

    interface 의 확장

--- cl-interfaces-01-starting-setup/src/app.ts
+++ cl-interfaces-01-starting-setup/src/app.ts
@@ -1,29 +1,32 @@
-// 클래스와 달리 interface 는 청사진으로 사용되지않으며
-// 사용자 정의 타입으로 사용할 뿐 입니다.
+// 상속과 인터페이스도 구현 하는법
+interface Named {
+  readonly name: string;
 
-// Person 객체가 가질 메소드를 정의 하는 것입니다
-interface Person {
-  name: string;
-  age: number;
-  greet(phrase: string): void;
-  // 이름 ,괄호 쌍, 반화타입, 입력후 void를 입력합니다
-  // 그후 괄호안에  원하는 입수를 입력합니다.
-  // 정리하자면 이눗의 이름과 타입, 이어서 메소드의 반환 타입 순서입니다
+  // readonly 제어도 추가 가능
+  // public private는 추가 불가능
+  // 즉 class 에도 영향을 미치게 됩니다.
 }
 
-let user1: Person
+interface Greetable extends Named {
+  // , 이용해 확장 가능하며 class는 불가능 합니다.
+  age: number;
+  greet(phrase: string): void;
+}
 
-user1 = {
-    name: 'Max,'
-    age: 30,
-    greet(phrase: string) {
-        console.log(phrase + ' ' + this.name)
-    }
-};
+class Person implements Greetable, Named {
+  name: string;
+  age = 30;
+  constructor(n: string) {
+    this.name = n;
+  }
 
-user1.greet('Hi there - I am')
+  greet() {
+    console.log(pharse + " " + this.name);
+  }
+}
 
-// Person 인터페이스로 할 수 있는것
-1. 객체의 타입을 확인 하는데 사용 할 수 있으며
-    user1이라는 변수를 입력하고  
-     
+let user1: Greetable;
+
+user1 = new Person("Max");
+
+user1.greet("Hi there - I am");
