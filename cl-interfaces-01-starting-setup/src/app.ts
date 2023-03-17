0870eb51b57f897a5d152c72eafeb489a06b680e
pung8146 <pung8146@naver.com>
2023-03-17 11:51:02 +0900

    인터페이스는 클래스와 달리 타입스크립트에만 있는 타입스크리전용 구조

--- cl-interfaces-01-starting-setup/src/app.ts
+++ cl-interfaces-01-starting-setup/src/app.ts
@@ -1,21 +1,18 @@
-
-인터페이스는 객체의 구조를 정의하지만
-함수의 구조를 정의 하는 데에도 사용됩니다. 
-인터페이스를 사용하여 함수를 생성할 수 있습니다.
 interface AddFn {
-    (a:number , b:number): number;
+  (a: number, b: number): number;
 }
 
-type AddFn = (a:number,b: number) => number;
+type AddFn = (a: number, b: number) => number;
 
 let add: AddFn;
 
-add = (n1: number, n2:number) => {
-    return n1 + n2
-}
+add = (n1: number, n2: number) => {
+  return n1 + n2;
+};
 
 interface Named {
   readonly name: string;
+  outputName?: string;
 }
 
 interface Greetable extends Named {
