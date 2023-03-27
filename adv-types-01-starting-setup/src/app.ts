917dd2297ee5009e57a7a648ba050c45b2d3f443
pung8146 <pung8146@naver.com>
2023-03-28 08:31:48 +0900

    유니온 타입 에 타입추론으로 생산이 높아지나 오류 발생도늘어나서 타입가드를 이용한다.

--- adv-types-01-starting-setup/src/app.ts
+++ adv-types-01-starting-setup/src/app.ts
@@ -56,4 +56,42 @@ function printEmployeeInformation(emp: UnknownEmployee) {
   }
 }
 
-printEmployeeInformation(e1);
+printEmployeeInformation({ name: "Manu", startDate: new Date() });
+
+class Car {
+  drive() {
+    console.log("Driving...");
+  }
+}
+
+class Truck {
+  drive() {
+    console.log("Driving a truck...");
+  }
+
+  loadCargo(amount: number) {
+    console.log("Loading cargo ..." + amount);
+  }
+}
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVehicle(Vehicle: Vehicle) {
+  vehicle.drive();
+  if (vehicle instanceof Truck) {
+    Vehicle.loadCargo(1000);
+  }
+  // instanceof는 바닐라 자바스크립트에 내장된 일반 연산자입니다.
+}
+
+// 타입가드는 특정 속성이나 메소드를 사용하기전에
+// 그것이 존재하는지 확인하거나 타입을 사용하기 전에 이타입으로
+// 어떤 작어블 수행할 수 있는지를확인 하는 개념 또는 방식을 나타내는 용어입니다.
+// 객체의 경우 instanceof나 in 을 사용하여 수행할 수 있고
+// 다른 타입들의 경우 typeof를 사용 할 수 있습니다
+// 유니언 타입이 주는 유연성을 사용하여
+// 런타임시 얻는 정확한 타입을 기반으로 하는 한가지 또는 다른 기능을 수행하는
+// 코드를 작성할 수 있게 해줍니다
