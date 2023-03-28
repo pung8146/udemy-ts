a8ca2f167e96e0ba4e2fe3d1ae64e07d3b049e56
pung8146 <pung8146@naver.com>
2023-03-28 15:52:38 +0900

    타입의 형변환 , DOM 에 대한 접근

--- adv-types-01-starting-setup/src/app.ts
+++ adv-types-01-starting-setup/src/app.ts
@@ -110,3 +110,13 @@ function moveAnimal(animal: Animal) {
 }
 
 moveAnimal({ type: "bird", flyingSpeed: 10 });
+
+const userInputElement = <HTMLInputElement>(
+  document.getElementById("user-input")!
+);
+
+const userInputElement = <HTMLInputElement>(
+  (document.getElementById("user-input")! as HTMLInputElement)
+);
+
+userInputElement.value = "Hi there";
