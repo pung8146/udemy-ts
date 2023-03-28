22317238a3c567b54e8cf6f725e1b558afc28f20
pung8146 <pung8146@naver.com>
2023-03-28 12:00:31 +0900

    객체와 유니언 타입을 사용한 패턴

--- adv-types-01-starting-setup/src/app.ts
+++ adv-types-01-starting-setup/src/app.ts
@@ -87,11 +87,26 @@ function useVehicle(Vehicle: Vehicle) {
   // instanceof는 바닐라 자바스크립트에 내장된 일반 연산자입니다.
 }
 
-// 타입가드는 특정 속성이나 메소드를 사용하기전에
-// 그것이 존재하는지 확인하거나 타입을 사용하기 전에 이타입으로
-// 어떤 작어블 수행할 수 있는지를확인 하는 개념 또는 방식을 나타내는 용어입니다.
-// 객체의 경우 instanceof나 in 을 사용하여 수행할 수 있고
-// 다른 타입들의 경우 typeof를 사용 할 수 있습니다
-// 유니언 타입이 주는 유연성을 사용하여
-// 런타임시 얻는 정확한 타입을 기반으로 하는 한가지 또는 다른 기능을 수행하는
-// 코드를 작성할 수 있게 해줍니다
+interface Bird {
+  flyingSpeed: number;
+}
+
+interface Horse {
+  runnigSpeed: number;
+}
+
+type Animal = Bird | Horse;
+
+function moveAnimal(animal: Animal) {
+  let speed;
+  switch (animal.type) {
+    case "bird":
+      speed = animal.flyingSpeed;
+      break;
+    case "horse":
+      speed = animal.runnigSpeed;
+  }
+  console.log("Moving with speed " + animal.speed);
+}
+
+moveAnimal({ type: "bird", flyingSpeed: 10 });
